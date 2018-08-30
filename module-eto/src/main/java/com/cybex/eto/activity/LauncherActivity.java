05dd971d03ea0baefa4e94dc1193440e3ff9ca27
michael-dzm <99054992@qq.com>
2018-08-30 15:24:46 +0800

    组件化配置

---
+++ module-eto/src/main/java/com/cybex/eto/activity/LauncherActivity.java
@@ -0,0 +1,19 @@
+package com.cybex.eto.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.cybex.basemodule.base.BaseActivity;
+
+public class LauncherActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onNetWorkStateChanged(boolean isAvailable) {
+
+    }
+}
