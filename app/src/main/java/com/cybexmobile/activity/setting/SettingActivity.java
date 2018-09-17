6898e6ebb3fa0be920aaa254993a791a67525eef
michael-dzm <99054992@qq.com>
2018-09-17 16:01:39 +0800

    fix kill process and reboot

--- app/src/main/java/com/cybexmobile/activity/setting/SettingActivity.java
+++ app/src/main/java/com/cybexmobile/activity/setting/SettingActivity.java
@@ -191,13 +191,16 @@ public class SettingActivity extends BaseActivity implements FrequencyModeDialog
     }
 
     public void restartApp() {
-//        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-//        manager.restartPackage("com.cybexmobile");
-        Intent intent = new Intent(getBaseContext(), SplashActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        getBaseContext().startActivity(intent);
-        android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(0);
+        showLoadDialog();
+        mTvServer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), SplashActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getBaseContext().startActivity(intent);
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        }, 2000);
     }
 
     private void displayLanguage() {
